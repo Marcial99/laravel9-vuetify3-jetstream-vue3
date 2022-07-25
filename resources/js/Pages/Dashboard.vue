5159ea535cc17d9abfb33142bc97bf1059a8cabf
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-07-25 11:14:16 -0500

    Implementación de vuetify en laravel

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Jetstream/Welcome.vue";
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight"></h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <v-alert color="info" icon="$info">
+                        I'm an info alert that uses <strong>color</strong> and
+                        <strong>icon</strong> props instead of
+                        <strong>type</strong>
+                    </v-alert>
+                    <Welcome />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    data() {
+        return {
+            dialog: false,
+        };
+    },
+};
+</script>
+
+<template>
+    <div class="text-center">
+        <v-btn color="primary" @click="dialog = true"> Open Dialog </v-btn>
+
+        <v-dialog v-model="dialog" fullscreen>
+            <v-card>
+                <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" block @click="dialog = false"
+                        >Close Dialog</v-btn
+                    >
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<style scoped></style>
